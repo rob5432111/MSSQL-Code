0640bd82e3b54645288fb239d477515adaa1e905
Roberto Pozo <roberto.pozo.andrade@gmail.com>
2021-09-07 15:21:31 -0500

    Added new sps for CTE and TempTable examples

---
+++ MSSQL-Code/Programmability/StoredProcedures/External/sp_BestSeller_Money_Monthly_TempTablesExample.sql
@@ -0,0 +1,34 @@
+﻿CREATE PROCEDURE [External].[sp_BestSeller_Money_Monthly_TempTablesExample]
+@StartDate DATE = NULL
+, @EndDate DATE = NULL
+AS
+BEGIN	
+
+	DECLARE @EndDatePlusOne DATE = (SELECT DATEADD(DAY, 1, @EndDate ))
+	
+	--Prevent error for the creation of the same table name
+	IF (OBJECT_ID('tempdb..#tmpSumTotalSold') IS NOT NULL)
+		DROP TABLE #tmpSumTotalSold;
+
+	SELECT SD.ProductId, EndOfMonth = EOMONTH(S.SaleDate), TotalProductsSold = SUM(SD.SaleDetailSubTotal) 
+	--Numerate the qty of products sold ordered descending and product id in case of a tie
+	, RN = ROW_NUMBER() OVER(PARTITION BY EOMONTH(S.SaleDate) 
+								ORDER BY SUM(SD.SaleDetailSubTotal) DESC
+										, SD.ProductId) --Tie Breaker
+	
+	INTO #tmpSumTotalSold --Create a temporal table to store results
+
+	FROM Sale S
+	JOIN SaleDetail SD ON SD.SaleId = S.SaleId
+	WHERE (@StartDate IS NULL OR S.SaleDate >= @StartDate)
+	AND (@EndDate IS NULL OR S.SaleDate < @EndDatePlusOne)
+	GROUP BY SD.ProductId, EOMONTH(S.SaleDate)
+	
+	SELECT [Year] = YEAR(tmp.EndOfMonth), [Month] = DATENAME(MONTH, tmp.EndOfMonth),C.CategoryName, P.ProductName, tmp.TotalProductsSold
+	FROM #tmpSumTotalSold tmp 
+	JOIN Product P ON P.ProductId = tmp.ProductId
+	LEFT JOIN Category C ON C.CategoryId = P.CategoryId 
+	WHERE RN = 1; --Select only the top 1 selling products
+
+END
+GO
---
+++ MSSQL-Code/Programmability/StoredProcedures/External/sp_BestSeller_Quantity_Monthly_CTEExample.sql
@@ -0,0 +1,31 @@
+﻿CREATE PROCEDURE [External].[sp_BestSeller_Quantity_Monthly_CTEExample]
+@StartDate DATE = NULL
+, @EndDate DATE = NULL
+AS
+BEGIN	
+
+	DECLARE @EndDatePlusOne DATE = (SELECT DATEADD(DAY, 1, @EndDate ))
+	
+	;WITH CTE_ProductSoldCount
+	AS 
+	(
+		SELECT SD.ProductId, EndOfMonth = EOMONTH(S.SaleDate), TotalProductsSold = SUM(SD.SaleDetailQuantity) 
+		--Numerate the qty of products sold ordered descending and product id in case of a tie
+		, RN = ROW_NUMBER() OVER(PARTITION BY EOMONTH(S.SaleDate) 
+									ORDER BY SUM(SD.SaleDetailQuantity) DESC
+											, SD.ProductId) --Tie Breaker
+		FROM Sale S
+		JOIN SaleDetail SD ON SD.SaleId = S.SaleId
+		WHERE (@StartDate IS NULL OR S.SaleDate >= @StartDate)
+		AND (@EndDate IS NULL OR S.SaleDate < @EndDatePlusOne)
+		GROUP BY SD.ProductId, EOMONTH(S.SaleDate)
+	)
+
+	SELECT [Year] = YEAR(CTE.EndOfMonth), [Month] = DATENAME(MONTH, CTE.EndOfMonth),C.CategoryName, P.ProductName, CTE.TotalProductsSold
+	FROM CTE_ProductSoldCount CTE 
+	JOIN Product P ON P.ProductId = CTE.ProductId
+	LEFT JOIN Category C ON C.CategoryId = P.CategoryId
+	WHERE RN = 1; --Select only the top 1 selling products
+
+END
+GO
